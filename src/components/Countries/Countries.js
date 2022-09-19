cb7266315685f72d0c77f31c89745c0d641b9b4a
Dave Fisher <dave.j.fish@gmail.com>
2022-09-19 16:25:52 -0700

    getting and displaying pictures from third party api

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,11 +1,10 @@
 import './Countries.css';
 
-export default function CountryCards({ continent, iso2, iso3, name, Local_name }) {
+export default function CountryCards({ continent, iso2, name, Local_name }) {
   return (
     <div className='card'>
-      <img src='http://placekitten.com/200/300' />
+      <img src={`https://flagcdn.com/${iso2.toLowerCase()}.svg`} />
       <span>{name}</span>
-      <span>{Local_name}</span>
       <span>{continent}</span>
     </div>
   );
