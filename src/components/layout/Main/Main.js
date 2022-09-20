e4b55641f593c266682925e785e77557d7e99549
Dave Fisher <dave.j.fish@gmail.com>
2022-09-20 15:16:49 -0700

    rewrote filter with switch case and updated state to handle querrying by name or continent

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -3,21 +3,26 @@ import CountryCards from '../../Countries/Countries';
 import { useCountries } from '../../../hooks/useCountries';
 
 export default function Main() {
-  const { filterCountries, setContinent, error } = useCountries();
+  const { filterCountries, setQuery, searchParam, setSearchParam, error } = useCountries();
   return (
     <main>
       <h2>Countries</h2>
       <span className='error'>{error}</span>
       <div className='search'>
+        <select value={searchParam} onChange={(e) => setSearchParam(e.target.value)}>
+          <option value=''>-</option>
+          <option value='continent'>continent</option>
+          <option value='name'>name</option>
+        </select>
         <label>
-          search by continent: 
+          search: 
           <input onChange={(e) => {
-            setContinent(e.target.value.toLowerCase());
+            setQuery(e.target.value.toLowerCase());
           }}></input>
         </label>
       </div>
       <div className='autoCards'>
-        {filterCountries().map((country) => (
+        {filterCountries(searchParam).map((country) => (
           <CountryCards key={country.id} {...country} />
         ))}
       </div>
--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -3,7 +3,8 @@ import { fetchCountries } from '../services/countries';
 
 export function useCountries() {
   const [countries, setCountries] = useState([]);
-  const [continent, setContinent] = useState('');
+  const [query, setQuery] = useState('');
+  const [searchParam, setSearchParam] = useState('');
   const [error, setError] = useState('');
 
   useEffect(() => {
@@ -18,12 +19,22 @@ export function useCountries() {
     fetchData();
   }, []);
 
-  const filterCountries = () => {
-    return (continent === '') ? countries : countries.filter((country) => {
-      if (country.continent)
-        return country.continent.toLowerCase().includes(continent);
-    });
+  const filterCountries = (param) => {
+    switch (param) {
+      case '':
+        return countries;
+      case 'continent':
+        return countries.filter((country) => {
+          if (country.continent)
+            return country.continent.toLowerCase().includes(query);
+        });
+      case 'name':
+        return countries.filter((country) => {
+          if (country.name)
+            return country.name.toLowerCase().includes(query);
+        });
+    }
   };
 
-  return { filterCountries, setContinent, error };
+  return { filterCountries, setQuery, searchParam, setSearchParam, error };
 }
